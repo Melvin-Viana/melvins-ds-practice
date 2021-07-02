c5e7c6ba59e62655c1f40e2cb0b3abd5dd11d6fc
Melvin Viana <mjviana057@gmail.com>
2021-07-02 10:51:29 -0700

    BubbleSort

--- udemy-bootcamp/Sorts/BubbleSort.js
+++ udemy-bootcamp/Sorts/BubbleSort.js
@@ -10,9 +10,28 @@ const bubbleSort = (arr) => {
                 noSwaps = false;
             }
         }
-        if (noSwaps) break;
+        if (noSwaps) return arr;
     }
     return arr;
 }
 
+// const bubbleSort = (arr) => {
+//     for (let i = 0; i < arr.length-1; i++) {
+//         let noSwaps = true;
+//         for (let j = 0; j < arr.length - i; j++) {
+//             if (arr[j] < arr[j-1]) {
+//                 let temp = arr[j];
+//                 arr[j] = arr[j-1];
+//                 arr[j-1] = temp;
+//                 noSwaps = false;
+//             }
+//         }
+//         console.log(arr)
+//         if (noSwaps) return arr;
+//     }
+//     return arr;
+// }
+
+
 console.log(bubbleSort([5,3,4,1,2]))
+console.log(bubbleSort([6,5,4,3,2,1]))
--- udemy-bootcamp/Sorts/sort.md
+++ udemy-bootcamp/Sorts/sort.md
@@ -1,4 +1,11 @@
 # Takeaways
+## Quadratic
 - "QUADRATIC" Sorts b/c all of them take an average of O(N^2) Time.
 - Better w/ small data sets
 - Insertion sort is great sorting an array when inserting in an array and sorting it in real time.
+
+- Bubble Sort => Sorting ascending, and doing consecutive swaps and bubbling the greatest values at the end
+- Selction Sort => Sorted ascending, doing single swaps on each iteration, placing the lowest values in the beginning
+- Insertion SOrt => From left to right, examine each item and compare it to the items on its left. Insert the item to its correct location.n   
+
+## Intermediate Algorithms
